0e3211f3151e4f3118cf7d0a10125cc276a68b5a
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2024-09-12 02:57:42 -0500

    data

---
+++ Lab2/task2.py
@@ -0,0 +1,11 @@
+"""
+Task1, Lab2, Modelado Simulacion y optimización 
+Uniandes
+Developed by:
+@Johan Alexis Bautista Quinayas & @Danny Camilo Muñoz Sanabria
+"""
+
+from pyomo.environ import *
+
+model = ConcreteModel()
+
