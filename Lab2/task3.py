43db364a7f6dfe637de3fa502dc65a5a601b59df
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2024-09-18 17:12:31 -0500

    3ok

--- Lab2/task3.py
+++ Lab2/task3.py
@@ -10,27 +10,26 @@ import pandas as pd
 
 model = ConcreteModel()
 
+# Carga de datos
 installation_costs = pd.read_csv('data/installation_costs.csv')
 communication_costs = pd.read_csv('data/communication_costs.csv')
 energy_consumption = pd.read_csv('data/energy_consumption.csv')
 sensor_coverage = pd.read_csv('data/sensor_coverage.csv')
 
+# Transformar datos en diccionarios
 installation_costs = dict(zip(installation_costs['Location'], installation_costs['InstallationCost']))
 energy_consumption = dict(zip(energy_consumption['SensorType'], energy_consumption['EnergyConsumption']))
-
-locations = set(['L1', 'L11', 'L4', 'L5', 'L7', 'L2', 'L10', 'L12', 'L3', 'L9', 'L6', 'L8'])
-sensors = set(['S1','S2','S3'])
-
 communication_costs = (
     communication_costs.groupby('Location')
     .apply(lambda x: dict(zip(x['SensorType'], x['CommunicationCost'])))
-    .reset_index(name='Costs')  
+    .to_dict()
 )
 
-communication_costs = communication_costs.set_index('Location')['Costs'].to_dict()
-
+# Transformar sensor_coverage para crear el parámetro delta
 sensor_coverage = sensor_coverage.set_index('Location').T.to_dict()
+delta = {(l, s): sensor_coverage[l].get(s, 0) for l in sensor_coverage for s in sensor_coverage[l]}
 
+# Adjacencias
 Adj = {
     'L1': {'L1', 'L2', 'L3', 'L5'},
     'L2': {'L2', 'L1', 'L5'},
@@ -46,58 +45,32 @@ Adj = {
     'L12': {'L12', 'L7', 'L8', 'L9'}
 }
 
-"""print('locations:')
-print(locations)
-print('Sensors:')
-print(sensors)
-print('Installation costs:')
-print(installation_costs)
-print('Energy consumption:')
-print(energy_consumption)
-print('Communication cost:')
-print(communication_costs)
-print('Sensor coverage:')
-print(sensor_coverage)
-print('Matriz de adjacency:')
-print(Adj)
-"""
-
 # Conjuntos
-model.L = Set(initialize=sorted(locations))  
-model.S = Set(initialize=sorted(sensors))
+model.L = Set(initialize=sorted(installation_costs.keys()))  
+model.S = Set(initialize=sorted(energy_consumption.keys()))
 
-# Parametros
+# Parámetros
 model.installation_costs = Param(model.L, initialize=installation_costs)
 model.energy_consumption = Param(model.S, initialize=energy_consumption)
-
-def get_comm_cost(model, l, s):
-    return communication_costs[l][s]
-model.communication_costs = Param(model.L, model.S, initialize=get_comm_cost)
-
-def get_sensor_cov(model, l, s):
-    return sensor_coverage[l][s]
-model.sensor_coverage = Param(model.L, model.S, initialize=get_sensor_cov)
+model.communication_costs = Param(model.L, model.S, initialize=lambda model, l, s: communication_costs[l][s])
+model.delta = Param(model.L, model.S, initialize=lambda model, l, s: delta[(l, s)])
 
 # Variables de decisión
 model.x = Var(model.L, model.S, within=Binary)
 
+# Función objetivo
 def objective_rule(model):
-    return sum((model.installation_costs[l] + model.energy_consumption[s] + model.communication_costs[l, s]) * model.x[l, s] 
-               for l in model.L for s in model.S)
+    return sum((model.installation_costs[l] + model.energy_consumption[s] + model.communication_costs[l, s]) * model.x[l, s] for l in model.L for s in model.S)
 model.Objective = Objective(rule=objective_rule, sense=minimize)
 
-# Restricción: la necesidad de un sensor específico en cada localidad debe ser satisfecha
+# Restricción de cobertura
 def sensor_need_rule(model, l, s):
-    if sensor_coverage[l][s] == 1:  
-        return sum(model.x[k, s] for k in Adj[l].union({l})) >= 1
-    else:
-        return Constraint.Skip  
-
+    return sum(model.x[k, s] * model.delta[l, s] for k in Adj[l].union({l})) >= model.delta[l, s]
 model.Sensor_Need_Constraint = Constraint(model.L, model.S, rule=sensor_need_rule)
 
+# Resolución del modelo
 solver = SolverFactory('glpk')
 solver.solve(model)
-    
 model.display()
 
 def print_results(model):
